c3854110caee5113a150f17847b840550e2d29cc
Misha <m.mazaixin@yandex.ru>
2024-07-07 15:13:14 +0300

    Rename func telegram_bot.py
    Переименовал функцию согласно стандарту

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,11 +7,8 @@ from random import shuffle
 from time import sleep
 
 
-TOKEN = 'API_TELEGRAM_TOKEN'
-CHAT_ID = 'CHAT_ID_TELEGRAM'
 IMG_PATH = 'images/'
 
-ARGS_INFO = 'Publishes posts in telegram chat'
 TIME = {'TIME_NAME': 'time', 'TIME_TYPE': int, 'TIME_DEFAULT': 14400, 'TIME_HELP_MSG': 'Введите время публикации поста'}
 DIRECTORY = {'DIRECTORY_NAME': 'directory', 'DIRECTORY_TYPE': str, 'DIRECTORY_DEFAULT': 'images', 'DIRECTORY_HELP_MSG': 'Введите имя директории, где хранятся фотографии'}
 
@@ -35,24 +32,25 @@ def publish_post(bot: dict, chat_id: str, images: list, cycles_count: int, time:
     sleep(time)
 
 
-def prepares_images(time: int, directory: str, token: str, chat_id: str) -> None:
+def publish_images_from_directory(time: int, directory: str, token: str, chat_id: str) -> None:
     '''
-    Предоставляет доступ к боту и подготавливает фотографии для публикации
+    Публикует фотографии в Телеграмм канал из выбранной директории
         Параметры:
             time (int): Частота публикации постов
             directory (str): Путь к файлу, где хранятся фото для публикации
+            token (str): Ключ доступа к Телеграмм боту
+            chat_id (str): ID чата
     '''
-    chat_id = chat_id
     bot = Bot(token)
 
     images = os.listdir(directory)
     shuffle(images)
 
-    count_img = len(images)
+    img_count = len(images)
     cycles_count = 0
 
     while True:
-        if cycles_count == count_img:
+        if cycles_count == img_count:
             shuffle(images)
             cycles_count = 0
 
@@ -65,15 +63,15 @@ def main():
     Позволяет работать из командной строки
     '''
 
-    token = os.environ[TOKEN]
-    chat_id = os.environ[CHAT_ID]
+    token = os.environ['API_TELEGRAM_TOKEN']
+    chat_id = os.environ['CHAT_ID_TELEGRAM']
 
-    parser = argparse.ArgumentParser(description=ARGS_INFO)
+    parser = argparse.ArgumentParser(description='Publishes posts in telegram chat')
     parser.add_argument(TIME['TIME_NAME'], type=TIME['TIME_TYPE'], nargs='?', default=TIME['TIME_DEFAULT'], help=TIME['TIME_HELP_MSG'])
     parser.add_argument(DIRECTORY['DIRECTORY_NAME'], type=DIRECTORY['DIRECTORY_TYPE'], nargs='?', 
                         default=DIRECTORY['DIRECTORY_DEFAULT'], help=DIRECTORY['DIRECTORY_HELP_MSG'])
     args = parser.parse_args()
-    prepares_images(args.time, args.directory, token=token, chat_id=chat_id)
+    publish_images_from_directory(args.time, args.directory, token=token, chat_id=chat_id)
 
 
 if __name__ == '__main__':
