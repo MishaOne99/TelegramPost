d1a1e20c61133d4532d072a357b1b0b3df4311c9
Misha <m.mazaixin@yandex.ru>
2024-07-10 15:15:52 +0300

    Renaming constants nasa_img_service.py

--- nasa_img_service.py
+++ nasa_img_service.py
@@ -1,6 +1,6 @@
 from download_and_checks_image import download_image, outputs_image_format
 from requests import get
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlencode
 
 DEMO_KEY = 'DEMO_KEY'
 NASA_APOD_API = 'https://api.nasa.gov/planetary/apod'
@@ -8,7 +8,7 @@ NASA_EPIC_API = 'https://api.nasa.gov/EPIC/api/natural/images'
 EPIC_ARCHIVE = 'https://api.nasa.gov/EPIC/archive/natural'
 JPG_FORMAT = '.jpg'
 PNG_FORMAT = '.png'
-NASA_PATH = 'images/nasa_'
+APOD_PATH = 'images/apod_'
 EPIC_PATH = 'images/epic_'
 
 
@@ -21,7 +21,7 @@ def download_images_apod(count: int, api_key: str = DEMO_KEY) -> None :
     for num, link in enumerate(response.json()):
         if outputs_image_format(link['url']) != JPG_FORMAT:
             continue
-        download_image(link['url'], f'{NASA_PATH}{num}{JPG_FORMAT}')
+        download_image(link['url'], f'{APOD_PATH}{num}{JPG_FORMAT}')
 
 
 def download_images_epic(count: int, api_key: str = DEMO_KEY) -> None:
@@ -31,10 +31,10 @@ def download_images_epic(count: int, api_key: str = DEMO_KEY) -> None:
     response = get(NASA_EPIC_API, params=payload)
     response.raise_for_status()
 
-    for num, data in enumerate(response.json()):
+    for num, date_and_image in enumerate(response.json()):
         epic_images[num] = {
-            'date': data['date'][:10],
-            'image': data['image']
+            'date': date_and_image['date'][:10],
+            'image': date_and_image['image']
         }
 
     for num in range(count):
@@ -42,7 +42,8 @@ def download_images_epic(count: int, api_key: str = DEMO_KEY) -> None:
         image_id = epic_images[num]['image']
 
         year, month, day = str(date_image).split('-')
-
-        image_url = urlparse(f'{EPIC_ARCHIVE}/{year}/{month}/{day}/png/{image_id}{PNG_FORMAT}?api_key={api_key}')
-
+        
+        query = urlencode(payload)
+        image_url = urlparse(f'{EPIC_ARCHIVE}/{year}/{month}/{day}/png/{image_id}{PNG_FORMAT}?{query}')
+        
         download_image(image_url.geturl(), f'{EPIC_PATH}{num}{PNG_FORMAT}')
